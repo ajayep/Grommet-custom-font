dd5c514cdc192a22691a7108904ae3e7e3b134bc
ajayep <ajay@actiotech.com>
2020-07-16 08:18:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Grommet, Heading, Box, TextInput } from "grommet";
+
+import "./styles.css";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto Condensed",
+      size: "14px"
+    }
+  }
+};
+
+function App() {
+  const [text, setText] = useState("Sample");
+  return (
+    <Grommet theme={theme}>
+      <Box>
+        <Heading level={4}>Make it Right</Heading>
+        <TextInput
+          placeholder="Add text here"
+          onChange={() => setText(event.target.value)}
+        />
+        <p>{text}</p>
+      </Box>
+    </Grommet>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
